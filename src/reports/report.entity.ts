453f6c6a0b1db0c633bba5f08833ccc99cce9cc3
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-07-13 01:12:29 +0430

    feat: implement report creation and its assocaition with users

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../users/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Report {
@@ -7,4 +8,19 @@ export class Report {
 
   @Column()
   price: number;
+  @Column()
+  make: string;
+  @Column()
+  model: string;
+  @Column()
+  year: number;
+  @Column()
+  mileage: number;
+  @Column()
+  lng: number;
+  @Column()
+  lat: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
